5f2f5167746abe45b921a683cbbc6970ec9b1066
Dmytro <dmitry.k792@gmail.com>
2020-12-10 12:18:51 +0200

    initial commit

---
+++ MovDB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovDB
+//
+//  Created by Dmytro Kozak on 12/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
